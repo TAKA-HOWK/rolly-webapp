16950fd0252d5b4a28351c1b4e5c651f9b33f967
TAKA-HOWK <h.o.w.k323f323@gmail.com>
2026-03-01 13:47:09 +0300

    Updated MainActivity.kt file with new content

--- android/app/src/main/java/com/rolly/webapp/MainActivity.kt
+++ android/app/src/main/java/com/rolly/webapp/MainActivity.kt
@@ -1,12 +1,57 @@
 package com.rolly.webapp
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.webkit.WebSettings
+import android.webkit.PermissionRequest
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    // Other existing code...
+    private lateinit var webView: WebView
 
-    // Replace deprecated constant
-    private val mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    // Other existing code...
+        webView = WebView(this)
+        setContentView(webView)
+
+        // Основные настройки
+        webView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = true
+            databaseEnabled = true
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            setSupportMultipleWindows(true)
+            // Разрешаем доступ к файлам
+            allowFileAccess = true
+            allowContentAccess = true
+        }
+
+        // WebViewClient с поддержкой навигации
+        webView.webViewClient = WebViewClient()
+
+        // WebChromeClient с поддержкой permissions и консоли
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                if (request != null) {
+                    request.grant(request.resources)
+                }
+            }
+        }
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
